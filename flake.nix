08486bd5962f6eb4946019ebe6752f2e4dc654ee
Maxime Laudrin <maxime.laudrin@protonmail.com>
2025-12-02 13:03:21 +0100

    Add flake.nix adding scripts in the PATH

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  description = "Repository dev tools";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          shellHook = ''
+            export PATH=$PATH:${toString ./scripts}
+            export VAULT_DIR=$(pwd)
+          '';
+        };
+      }
+    );
+}
